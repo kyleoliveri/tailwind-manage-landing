e60d53bfa5eef438b636bedd6b4bc7a071c97032
Kyle Oliveri <kyle@Kyles-MacBook-Air.local>
2022-05-04 23:14:45 -0700

    Finished landing page and javascript logic

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       sm: '480px',
       md: '768px',
       lg: '976px',
-      xl: '1440px'
+      xl: '1440px',
     },
     extend: {
       colors: {
@@ -14,10 +14,10 @@ module.exports = {
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)'
-      }
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
     },
   },
   plugins: [],
